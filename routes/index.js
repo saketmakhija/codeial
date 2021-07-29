1d0a7d8775ba9be688abcacdc2829bf8fc940c0a
saket <sktmakhija@gmail.com>
2021-07-29 23:58:10 +0530

    add a controller and router for home

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
 const route = express.Router();
+
+const homeController = require('../controllers/home_controller');
 console.log("route is running");
+
+route.get('/', homeController.home);
 module.exports = route;
+
