bdda47aceb8aa662e39287ad8e562ce2070bad29
saket <sktmakhija@gmail.com>
2021-08-14 14:11:06 +0530

    add feature delete post and comment

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 module.exports.create = function(req, res){
     Post.create({
         content: req.body.content,
@@ -8,5 +9,19 @@ module.exports.create = function(req, res){
             console.log("error in posting"); return;
         }
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post: req.param.id}, function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
     });
 }
